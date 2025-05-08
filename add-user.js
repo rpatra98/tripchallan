5c90d15fb8bbd36e899798e25268ab062ac4318e
NuclearTeddy22 <nuclearteddy98@gmail.com>
2025-05-08 06:56:23 +0530

    Add add-user.js script to seed superadmin user

---
+++ add-user.js
@@ -0,0 +1,23 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+
+async function main() {
+  const prisma = new PrismaClient();
+  const hashedPassword = await bcrypt.hash('superadmin123', 12);
+  await prisma.user.create({
+    data: {
+      name: 'Super Admin',
+      email: 'superadmin@cbums.com',
+      password: hashedPassword,
+      role: 'SUPERADMIN',
+      coins: 1000000
+    }
+  });
+  await prisma.$disconnect();
+  console.log('User created!');
+}
+
+main().catch(e => {
+  console.error(e);
+  process.exit(1);
+}); 
